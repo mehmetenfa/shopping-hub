6e52d64aee2d8b22491ec3bb70e8c2d13b2b8121
qxan <mehmet.enfa@gmail.com>
2023-03-26 14:26:32 +0300

    appjs categorySlice

--- src/App.js
+++ src/App.js
@@ -1,17 +1,25 @@
-import './App.scss'
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import "./App.scss";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 // pages
-
+import { Home, Category, Cart } from "./pages/index";
 // components
+import Navbar from "./components/Navbar/Navbar";
+import Footer from "./components/Footer/Footer";
+import { Provider } from "react-redux";
+import store from "./store/store";
 
 function App() {
   return (
     <div className="App">
-      <Provider>
+      <Provider store={store}>
         <BrowserRouter>
+          <Navbar />
           <Routes>
-            <Route path='' element={ff} />
+            <Route path="/" element={<Home />} />
+            <Route path="/category/:id" element={<Category />} />
+            <Route path="/cart" element={<Cart />} />
           </Routes>
+          <Footer />
         </BrowserRouter>
       </Provider>
     </div>
