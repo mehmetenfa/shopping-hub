343ee4424704b2d30d453491ad3cbbb899f49da5
qxan <mehmet.enfa@gmail.com>
2023-03-27 15:03:08 +0300

    slider

--- src/components/Slider/Slider.js
+++ src/components/Slider/Slider.js
@@ -1,8 +1,16 @@
 import React from 'react'
+import './Slider.scss'
+import 'slick-carousel/slick/slick.css'
+import 'slick-carousel/slick/slick-thene.css'
+import { sliderImages } from '../../utils/images'
 
 const Slider = () => {
   return (
-    <div>Slider</div>
+    <div className='hero-slider'>
+      <div className='hero-slider-item'>
+        <img src={sliderImages[0]} alt='' />
+      </div>
+    </div>
   )
 }
 
