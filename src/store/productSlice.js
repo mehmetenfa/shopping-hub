e5496902b06cca0e85df350c7ae17c6ef233cde2
qxan <mehmet.enfa@gmail.com>
2023-03-28 14:47:23 +0300

    productSliceSaved

--- src/store/productSlice.js
+++ src/store/productSlice.js
@@ -1,37 +1,37 @@
 import { createSlice } from "@reduxjs/toolkit";
-import { STATUS } from '../utils/status'
-import { BASE_URL } from '../utils/apiUrl'
+import { BASE_URL } from "../utils/apiURL";
+import { STATUS } from "../utils/status";
 
 const productSlice = createSlice({
-    name: 'product',
-    initialState: {
-        data: [],
-        status: STATUS.IDLE,
-    },
+  name: "product",
+  initialState: {
+    data: [],
+    status: STATUS.IDLE,
+  },
 
-    reducers: {
-        setProducts(state, action){
-            state.data = action.payload;
-        },
-        setStatus(state, action){
-            state.status = action.payload;
-        },
+  reducers: {
+    setProducts(state, action) {
+      state.data = action.payload;
     },
+    setStatus(state, action) {
+      state.status = action.payload;
+    },
+  },
 });
 
 export const { setProducts, setStatus } = productSlice.actions;
 export default productSlice.reducer;
 
 export const fetchProducts = () => {
-  return async function fetchProductsThunk(dispatch){
+  return async function fetchProductThunk(dispatch) {
     dispatch(setStatus(STATUS.LOADING));
     try {
-        const response = await fetch(`${BASE_URL}products`);
-        const data = await response.json();
-        dispatch(setProducts(data));
-        dispatch(setStatus(STATUS.IDLE));
-    } catch(error) {
-        dispatch(setStatus(STATUS.ERROR));
+      const response = await fetch(`${BASE_URL}products`);
+      const data = await response.json();
+      dispatch(setProducts(data));
+      dispatch(setStatus(STATUS.IDLE));
+    } catch (error) {
+      dispatch(setStatus(STATUS.ERROR));
     }
-  }
-}
+  };
+};
