20ec8575ab76459e76e614570a6d9951ce2eb48c
qxan <mehmet.enfa@gmail.com>
2023-03-28 14:47:54 +0300

    storeIndexSaved

--- src/store/store.js
+++ src/store/store.js
@@ -1,16 +1,16 @@
 import { configureStore } from "@reduxjs/toolkit";
-import productReducer from './productSlice';
-import categoryReducer from './categorySlice';
-import modalReducer from './modalSlice';
-import cartReducer from './cartSlice';
+import productReducer from "./productSlice";
+import categoryReducer from "./categorySlice";
+import modalReducer from "./modalSlice";
+import cartReducer from "./cartSlice";
 
 const store = configureStore({
-    reducer: {
-        product: productReducer,
-        category: categoryReducer,
-        modal: modalReducer,
-        cart: cartReducer,
-    }
-})
+  reducer: {
+    product: productReducer,
+    category: categoryReducer,
+    modal: modalReducer,
+    cart: cartReducer,
+  },
+});
 
 export default store;
